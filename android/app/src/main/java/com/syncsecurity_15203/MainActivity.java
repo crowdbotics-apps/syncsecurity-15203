2cd0b00b7d61e8e8ea7104b13df2561934d3e1b0
Crowdbotics <team@crowdbotics.com>
2020-03-29 23:08:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/syncsecurity_15203/MainActivity.java
@@ -0,0 +1,15 @@
+package com.syncsecurity_15203;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "syncsecurity_15203";
+    }
+}
